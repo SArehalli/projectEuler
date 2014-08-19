a055a91bda4bb41b4f1f81bb4a3a4585fc42e92c
SArehalli <sgarehal@ucsd.edu>
2014-08-18 22:41:24 -0700

    Small Gains Have Been Made. Do Not Trust Complexity. Extra Lines Make Things Worse.

--- 096_SuDoku.py
+++ 096_SuDoku.py
@@ -20,9 +20,18 @@ def solved(puzzle):
                 return False
     return True
 
-def solve(puzzle):                                                      # Simple Sudoku
-    while not solved(puzzle):                                           # Is Solved in a Simple Way
-        for coord in [(i,j) for i in range(9) for j in range(9)]:       # Guessing is Harder
+'''
+    Simple is Harder
+    New Techniques create small gains
+    But closer beats far
+
+'''
+
+def simpleSolve(puzzle):
+    solutions = {}
+    prevState = puzzle
+    while not solved(puzzle):                                             
+        for coord in [(i,j) for i in range(9) for j in range(9)]:       
             if puzzle[coord[0]][coord[1]] == 0:
                 sols = list(range(1,10))
                 for number in puzzle[coord[0]]:
@@ -35,9 +44,46 @@ def solve(puzzle):                                                      # Simple
                     for number in row[3*int(coord[1]/3):3*(int(coord[1]/3)+1)]:
                         if number in sols:
                             sols.remove(number)
+                solutions[coord] = sols
                 if len(sols) == 1:
                     puzzle[coord[0]][coord[1]] = sols[0]
-    return puzzle
-
-for row in solve(grid[0]):
-    print(row)
+            else:
+                 solutions[coord] =[puzzle[coord[0]][coord[1]]]
+        for i in range(9):
+            for number in range(9):
+                count = 0
+                location = (i,-1)
+                for j in range(9):
+                    if number in solutions[(i,j)]:
+                        count += 1
+                        location = (i,j)
+                if count == 1:
+                    puzzle[location[0]][location[1]] = number
+        for i in range(9):
+            for number in range(9):
+                count = 0
+                location = (-1,i)
+                for j in range(9):
+                    if number in solutions[(j,i)]:
+                        count += 1
+                        location = (j,i)
+                if count == 1:
+                    puzzle[location[0]][location[1]] = number
+        for x in [(a,b) for a in range(3) for b in range(3)]:
+            for number in range(9):
+                count = 0
+                location = (-1,-1)
+                for y in [(a,b) for a in range(3 * x[0], 3 * (x[0]+1)) for b in range(3 * x[1], 3 * (x[1]+1))]:
+                    if number in solutions[y]:
+                        count += 1
+                        location = y
+                if count == 1:
+                    puzzle[location[0]][location[1]] = number
+        if puzzle == prevState:
+            break
+        prevState = puzzle
+    return puzzle    
+for puzzle in grid:
+    for row in simpleSolve(puzzle):
+        print(row)
+    print()
